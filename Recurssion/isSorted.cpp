12b636ddc5cf007a4eb3de95eb33c1bec6202055
Shahid Akhtar <shahidakhtar016@gmail.com>
2023-04-07 19:45:35 +0530

    recurssion isSorted solved

---
+++ Recurssion/isSorted.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Finding array is sorted using recurssion
+
+bool isSorted(int a[],int n){
+	if(n==0 || n==1)
+		return true;
+
+	if(a[0]<=a[1] && isSorted(a+1, n-1)){
+		return true;
+	}
+	return false;
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin>>n;
+	int arr[n];
+
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+
+	bool res = isSorted(arr, n);
+
+	if (res)
+		cout<<"Sorted Array";
+	else
+		cout<<"Not sorted array";
+	
+	return 0;
+}
